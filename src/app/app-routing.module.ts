db6c7b4668816f78b5d8b70d9a46138eb2d1db24
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-07-02 20:54:42 +0200

    Espace visiteurs - page d'une annonce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  { path: 'offers', loadChildren: () => import('./offers/offers.module').then(m => m.OffersModule)},
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: '**', redirectTo: 'home' }
--- src/app/services/offers.service.ts
+++ src/app/services/offers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, resolveForwardRef } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
@@ -49,8 +49,6 @@ export class OffersService {
       }
       this.dispatchOffers();
     });
-
-
     /* return new Observable(observer => {
       if(this.offers.length === 0 ){
         observer.error(new Error('Pas d\'offre enregistrée'));
@@ -70,6 +68,17 @@ export class OffersService {
     },2000);
   }); */
   //return this.offers;
+  }
+
+  getOfferById(offerId: string): Promise<Offer>{
+    return new Promise((resolve, reject) => {
+      this.db.database.ref(`offers/${offerId}`).once('value',(snapshot, err) => {
+        if(err){
+          reject(err);
+        }
+        resolve(snapshot.val());
+      })
+    })
   }
 
   getOffersOn(): void{
