fe219589230f176725fa443a0d1175844af057f7
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-07-02 17:35:30 +0200

    Modifier Mot de passe

--- src/app/account/edit-auth-infos/edit-auth-infos.component.ts
+++ src/app/account/edit-auth-infos/edit-auth-infos.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-edit-auth-infos',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAuthInfosComponent implements OnInit {
 
-  constructor() { }
+  @Input() currentUser!: User;
+
+  passwordForm!: FormGroup;
+
+  constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.initPasswordForm();
+  }
+
+  initPasswordForm(): void{
+    this.passwordForm = this.formBuilder.group({
+      oldPassword: ['', [Validators.required]],
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      newPasswordConfirm: ['', [Validators.required]]
+    })
+  }
+
+  onEditPassword(modal: any):void {
+    this.modalService.open(modal, {centered: true});
+  }
+
+  onSubmitPasswordForm(): void{
+    this.authService.signinUser(<string>this.currentUser.email, this.passwordForm.value.oldPassword)
+    .then(() => {
+      this.currentUser.updatePassword(this.passwordForm.value.newPassword)
+      .then(() => {
+        this.passwordForm.reset();
+        this.modalService.dismissAll();
+      }).catch(console.error);
+    }).catch(console.error);
   }
 
 }
