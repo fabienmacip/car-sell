59118609502bc0cede73085e275e4b66d36c3e7f
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-07-02 17:14:36 +0200

    Moidier EMAIL

--- src/app/account/edit-profile/edit-profile.component.ts
+++ src/app/account/edit-profile/edit-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -14,19 +15,29 @@ export class EditProfileComponent implements OnInit, OnChanges {
   @Output() messageEvent = new EventEmitter<string>();
 
   usernameForm!: FormGroup;
+  emailForm!: FormGroup;
 
   constructor(
     private modalService: NgbModal,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
       this.initUsernameForm();
+      this.initEmailForm();
   }
 
   initUsernameForm(): void{
     this.usernameForm = this.formBuilder.group({
       username: ['', [Validators.required]]
+    });
+  }
+
+  initEmailForm(): void{
+    this.emailForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
     });
   }
 
@@ -39,12 +50,31 @@ export class EditProfileComponent implements OnInit, OnChanges {
     this.modalService.open(modal, { centered: true});
   }
 
+  onEditEmail(modal: any): void{
+    this.emailForm.get('email')?.setValue(this.currentUser.email);
+    this.modalService.open(modal, { centered: true});
+  }
+
+
   onSubmitUsernameForm(): void{
     this.currentUser.updateProfile({displayName: this.usernameForm.value.username})
     .then(() => {
       this.modalService.dismissAll();
     }).catch(console.error);
   }
+
+  onSubmitEmailForm(): void{
+    this.authService.signinUser(<string>this.currentUser.email,this.emailForm.value.password)
+    .then(() => {
+      this.currentUser.updateEmail(this.emailForm.value.email)
+      .then(() => {
+        this.modalService.dismissAll();
+        this.emailForm.reset();
+    }).catch(console.error);
+    }).catch(console.error);
+  }
+
+
 
   // Utilisée pour tester le @Output
   onClick(){
