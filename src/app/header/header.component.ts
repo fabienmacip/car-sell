193cef5fc3a582489126bdd829e78508a887fcc8
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-07-02 13:43:14 +0200

    AUTH: deconnexion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  currentUserSubscription!: Subscription;
+  currentUser!: User;
+
   title = 'CarSell';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.currentUserSubscription = this.authService.currentUserSubject.subscribe({
+      next: user => this.currentUser = <User>user,
+      error: console.error
+    });
   }
 
   getTitle(): string {
     return this.title;
+  }
+
+  onSignout(): void{
+    this.authService.signoutUser()
+    .then(() => {
+      this.router.navigate(['/home']);
+    }).catch(console.error);
+  }
+
+  ngOnDestroy(): void{
+    this.currentUserSubscription.unsubscribe();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
+/* import { User } from '@angular/fire/auth'; */
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  currentUserSubject = new BehaviorSubject<User | null>(null);
+
   constructor(
     private auth: AngularFireAuth
-  ) { }
+  ) {
+    this.auth.onAuthStateChanged(user => {
+      this.currentUserSubject.next(user);
+    }, console.error);
+   }
 
   signupUser(email: string, password: string): Promise<any>{
     return new Promise((resolve, reject) => {
@@ -24,6 +33,17 @@ export class AuthService {
       this.auth.signInWithEmailAndPassword(email, password)
       .then(resolve).catch(reject);
     });
+  }
+
+  signoutUser(): Promise<void>{
+    return new Promise((resolve, reject) => {
+      this.auth.signOut()
+      .then(() => {
+        this.currentUserSubject.next(null);
+        resolve();
+      }).catch(reject);
+    });
+
   }
 
 }
